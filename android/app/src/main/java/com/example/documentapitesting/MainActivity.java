c106287b3a84602a52bac715318a97af08faf4ba
unknown <mam7264@gmail.com>
2020-07-21 23:38:58 +0530

    initial commit

---
+++ android/app/src/main/java/com/example/documentapitesting/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.documentapitesting;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main_dev.dart
@@ -0,0 +1,6 @@
+import 'package:documentapitesting/environment.dart';
+import 'package:documentapitesting/main_common.dart';
+
+Future<void> main() async{
+  await mainCommon(Environment.dev);
+}
